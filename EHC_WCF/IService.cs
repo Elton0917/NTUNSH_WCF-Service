50cda1612f491d3a844b018f1edba059152cb7b7
Elton Lee <bean800917@gmail.com>
2012-09-04 11:39:20 +0800

    updata api

---
+++ EHC_WCF/IService.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Runtime.Serialization;
+
+namespace EHC_WCF
+{
+
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string JSONData(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "getCase/{Case}")]
+        EHC_Case[] getCase(string Case);
+    }
+    [DataContract]
+    public class EHC_Case
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+    }
+}
---
+++ EHC_WCF/Service.svc.cs
@@ -0,0 +1,54 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EHC_WCF
+{
+    public class Service : IService
+    {
+        string Constring =
+        ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+        #region IRestServiceImpl Members
+
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public EHC_Case[] getCase(string Case)
+        {
+            using (con = new SqlConnection(Constring))
+            {
+                cmd = new SqlCommand("SELECT HCaseName FROM HomeCaseData WHERE HCaseName LIKE '" + Case + "%'", con);
+                sda = new SqlDataAdapter(cmd);
+                dt = new DataTable("Paging");
+                sda.Fill(dt);
+
+                int Rcount = dt.Rows.Count;
+                EHC_Case[] arrCase = new EHC_Case[Rcount];
+                int i = 0;
+                foreach (DataRow dr in dt.Rows)
+                {
+                    arrCase[i] = new EHC_Case();
+                    arrCase[i].Name = dr["HCaseName"].ToString();
+
+                    i++;
+                }
+                return arrCase;
+            }
+
+        }
+
+        #endregion
+    }
+}
